64f18a0b2cd786a77872e8867407c57a30260c76
XluoChen <cxluo@thoughtworks.com>
2017-07-23 10:57:39 +0800

    draw isosceles triangle by given number

--- sources/Asterisks.java
+++ sources/Asterisks.java
@@ -1,18 +1,21 @@
 public class Asterisks {
-    void printSingleAsterisks() {
+    void printSingleAsterisks () {
         System.out.print("*");
     }
+
     void drawHorizontalLine (int n) {
         for (int i = 0;i < n; i++) {
             this.printSingleAsterisks();
         }
     }
+
     void drawVerticalLine (int n) {
         for (int i = 0;i < n; i++) {
             this.printSingleAsterisks();
             System.out.println("");
         }
     }
+
     void drawRightTriangle (int n) {
         for (int i = 0;i < n; i++) {
             for (int j = 0;j <= i;j++) {
@@ -21,8 +24,21 @@ public class Asterisks {
             System.out.println("");
         }
     }
+
+    void drawIsoscelesTriangle (int n) {
+        for (int i = 0;i < n; i++) {
+            for (int k = 1;k < n-i;k++) {
+                System.out.print(" ");
+            }
+            for (int j = 0;j < (2*i+1);j++) {
+                this.printSingleAsterisks();
+            }
+            System.out.println("");
+        }
+    }
+
     public static void main (String[] args) {
         Asterisks asterisks = new Asterisks();
-        asterisks.drawRightTriangle(3);
+        asterisks.drawIsoscelesTriangle(3);
     }
 }
