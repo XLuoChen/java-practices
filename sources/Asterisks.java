6b5b3e2c97d6e76fd2e233c0a08adfe284ba4da2
XluoChen <cxluo@thoughtworks.com>
2017-07-23 11:23:45 +0800

    draw diamond with name by given number

--- sources/Asterisks.java
+++ sources/Asterisks.java
@@ -57,9 +57,30 @@ public class Asterisks {
             System.out.println("");
         }
     }
+    void drawDiamondWithName (int n) {
+        for (int i = 0;i < n/2; i++) {
+            for (int k = 1;k < n-i;k++) {
+                System.out.print(" ");
+            }
+            for (int j = 0;j < (2*i+1);j++) {
+                this.printSingleAsterisks();
+            }
+            System.out.println("");
+        }
+        System.out.println("Luo Chengxiu");
+        for (int i = n/2;i > 0; i--) {
+            for (int k = n-i;k > 0;k--) {
+                System.out.print(" ");
+            }
+            for (int j = (2*i-1);j > 0 ;j--) {
+                this.printSingleAsterisks();
+            }
+            System.out.println("");
+        }
+    }
 
     public static void main (String[] args) {
         Asterisks asterisks = new Asterisks();
-        asterisks.drawDiamond(7);
+        asterisks.drawDiamondWithName(7);
     }
 }
