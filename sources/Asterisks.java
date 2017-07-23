cb8906724a6574a73255e1f4b97bb9e9458fe0ae
XluoChen <cxluo@thoughtworks.com>
2017-07-23 10:24:58 +0800

    extract method to print single asterisk

---
+++ sources/Asterisks.java
@@ -0,0 +1,9 @@
+public class Asterisks {
+    void printSingleAsterisks() {
+        System.out.println("*");
+    }
+    public static void main (String[] args) {
+        Asterisks asterisks = new Asterisks();
+        asterisks.printSingleAsterisks();
+    }
+}
