a13c59b0e6ecf7495f8a0b5ed039969233f2be10
XluoChen <cxluo@thoughtworks.com>
2017-07-23 10:38:46 +0800

    draw horizontal line by given number

--- sources/Asterisks.java
+++ sources/Asterisks.java
@@ -1,9 +1,14 @@
 public class Asterisks {
     void printSingleAsterisks() {
-        System.out.println("*");
+        System.out.print("*");
+    }
+    void drawHorizontalLine (int n) {
+        for (int i = 0;i < n; i++) {
+            this.printSingleAsterisks();
+        }
     }
     public static void main (String[] args) {
         Asterisks asterisks = new Asterisks();
-        asterisks.printSingleAsterisks();
+        asterisks.drawHorizontalLine(8);
     }
 }
