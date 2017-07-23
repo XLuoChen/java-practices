9b30cd02c4266afd9121d7f717589339b4fa0b82
XluoChen <cxluo@thoughtworks.com>
2017-07-23 12:55:05 +0800

    output prime factors of given integer

--- sources/PrimeFactors.java
+++ sources/PrimeFactors.java
@@ -1,3 +1,25 @@
-public class GivenInteger {
-    public static void main ()
+import java.util.ArrayList;
+
+public class PrimeFactors {
+    void generate (int n) {
+        ArrayList<Integer> factors = new ArrayList<Integer>();
+        for (int i = 2;i < n/i;i++) {
+            if (n % i == 0) {
+                factors.add(i);
+                n = n / i;
+            }
+        }
+        if (n > 1) {
+            factors.add(n);
+        }
+
+        for (Integer item : factors) {
+            System.out.println(item);
+        }
+    }
+
+    public static void main(String[] args) {
+        PrimeFactors primeFactors = new PrimeFactors();
+        primeFactors.generate(60);
+    }
 }
